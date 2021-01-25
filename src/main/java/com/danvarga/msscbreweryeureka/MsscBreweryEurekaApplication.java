937e32d4dee48cf321e6a9c8029f9b6f6542e3c0
danvarga1985 <danvarga1985@gmail.com>
2021-01-25 15:18:17 +0100

    Basic configuration

--- src/main/java/com/danvarga/msscbreweryeureka/MsscBreweryEurekaApplication.java
+++ src/main/java/com/danvarga/msscbreweryeureka/MsscBreweryEurekaApplication.java
@@ -2,7 +2,10 @@ package com.danvarga.msscbreweryeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+// The only configuration needed.
+@EnableEurekaServer
 @SpringBootApplication
 public class MsscBreweryEurekaApplication {
 
